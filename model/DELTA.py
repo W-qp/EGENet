b5e6d4a62264ae187ba5b572a31a4b14e8eebde2
wqp <110106824+W-qp@users.noreply.github.com>
2023-07-24 20:15:01 +0800

    Update DELTA.py

--- model/DELTA.py
+++ model/DELTA.py
@@ -64,8 +64,8 @@ class DELTA(nn.Module):
         self.decoder = FPN(encoder_channels, 4, decode_channels, n_classes)
 
     def forward(self, x):
-        x = x if isinstance(x, tuple) else tuple(x)
-        x = torch.cat(x, dim=1)
+        # x = x if isinstance(x, tuple) else tuple(x)
+        # x = torch.cat(x, dim=1)
         h, w = x.shape[2:]
 
         x = self.backbone(x)
